8877527690ab2092c8b2a00057762448cf3946e7
minerupset <35759470+minerupset@users.noreply.github.com>
2020-05-14 15:11:06 -0700

    Adds a profile whenever we create user. Also activates signup and login

--- main_app/urls.py
+++ main_app/urls.py
@@ -4,4 +4,6 @@ from . import views
 urlpatterns = [
 path('', views.home, name='home'),
 path('events/', views.events_index, name='index'),
+#Auth related paths
+path('accounts/signup', views.signup, name='signup')
 ]
--- main_app/views.py
+++ main_app/views.py
@@ -1,5 +1,5 @@
-from django.shortcuts import render
-from .models import Event
+from django.shortcuts import render, redirect
+from .models import Event, Profile
 from django.contrib.auth import login
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth.forms import UserCreationForm
@@ -10,7 +10,23 @@ from django.contrib.auth.forms import UserCreationForm
 def home(request):
     return render(request, 'home.html')
 
-
+@login_required
 def events_index(request):
     events = Event.objects.all()
     return render(request, 'events/index.html', {'events': events})
+
+def signup(request):
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            # This creates our new Profile linked to the User object
+            new_profile = Profile(user=user)
+            new_profile.save()
+            login(request, user)
+            return redirect('login')  
+        else:
+            pass
+    form = UserCreationForm()
+    context = {'form': form}
+    return render(request, 'registration/signup.html', context)
