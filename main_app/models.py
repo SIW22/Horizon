916938adc7598abcd623298f8d24c46a56cb3841
minerupset <35759470+minerupset@users.noreply.github.com>
2020-05-14 11:34:00 -0700

    Basic migrations set up for events

--- main_app/models.py
+++ main_app/models.py
@@ -1,9 +1,16 @@
 from django.db import models
+from datetime import date
+from django.contrib.auth.models import User
 
 # Create your models here.
+
+
 class Event(models.Model):
-    name = models.CharField(max_length=50)
-    where = models.CharField(max_length=50)
+    title = models.CharField(max_length=255)
+    where = models.CharField(max_length=255)
+    start_date = models.DateField()
+    # If end date is blank, use start_date
+    end_date = models.DateField()
 
     def __str__(self):
-        return self.name
+        return f"{self.title} - {self.start_date}"
