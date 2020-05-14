ae3a9e35d301a1b38b13dda06c84a0106f7fe810
minerupset <35759470+minerupset@users.noreply.github.com>
2020-05-14 14:32:18 -0700

    Adds base html and beginning of styling and client side JS

--- main_app/views.py
+++ main_app/views.py
@@ -1,5 +1,8 @@
 from django.shortcuts import render
 from .models import Event
+from django.contrib.auth import login
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth.forms import UserCreationForm
 
 # Create your views here.
 
