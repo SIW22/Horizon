a61b0c8edc712cef5736c48f4e0ee0c59ec6261d
minerupset <35759470+minerupset@users.noreply.github.com>
2020-05-18 10:33:30 -0700

    updating model migrations

--- main_app/static/scripts/app.js
+++ main_app/static/scripts/app.js
@@ -1,22 +1,9 @@
-// import { getCookie } from "./shared.js";
+import { updateCountdowns } from "./timerMethods.js";
 
-// const mainEl = document.querySelector("main");
-// mainEl.addEventListener("click", (event) => {
-//   //Delete Event
-//   if (event.target.classList.contains("delete-event")) {
-//     const button = event.target;
-//     let csrftoken = getCookie("csrftoken");
-//     fetch(`/events/${button.getAttribute("data-eventid")}/remove_event`, {
-//       method: "DELETE",
-//       headers: { "X-CSRFToken": csrftoken },
-//     })
-//       .then((response) => {
-//         if (response.status == 200) {
-//           window.location.href = window.location;
-//         } else {
-//           window.location.href = window.location;
-//         }
-//       })
-//       .catch();
-//   }
-// });
+function updateNavTimer() {
+  const mainTimer = document.querySelector(".main-countdown");
+  mainTimer.setAttribute("data_targetdate", "2020-05-18");
+  updateCountdowns();
+}
+
+updateNavTimer();
